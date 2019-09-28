37e25265994e1f01cd2044c6bd54817b55e6aafb
Felix Labelle <flx.labelle@gmail.com>
2019-09-27 22:04:24 -0400

    Removed unnecessary call and fixed minor errors

--- spectrogramDemo.py
+++ spectrogramDemo.py
@@ -5,20 +5,21 @@ import numpy as np
 
 CHUNK = 1024
 CHANNELS = 1
-RATE = 10000
+SAMPLING_FREQ = 10000
 
 # TODO: fix delay that accumulates
+# https://matplotlib.org/api/animation_api.html#module-matplotlib.animation
 # TODO: figure out how to kill stream when windows closes
 try:
 	print("Starting stream")
 	p = pyaudio.PyAudio()
 	stream = p.open(format=pyaudio.paInt16,
 					channels=CHANNELS,
-					rate=RATE,
+					rate=SAMPLING_FREQ,
 					input=True,
 					frames_per_buffer=CHUNK)
 
-	print("The frequential resolution is ", (RATE/2)/(2*CHUNK/CHUNK))
+	print("The frequential resolution is ", (2*CHUNK/SAMPLING_FREQ), " Hz per bin")
 	# Should I increase the chunk size?
 	# Is putting the last data worth it?
 	plt.ion()
@@ -26,15 +27,16 @@ try:
 	while True:
 		byteStream = stream.read(CHUNK)  #read audio stream
 		data = np.frombuffer(byteStream, dtype=np.int16)
-		f, t, Sxx = spectrogram(np.append(prevData,data), fs=RATE)
+		f, t, Sxx = spectrogram(np.append(prevData,data), fs=SAMPLING_FREQ)
 		prevData = data
 		plt.pcolormesh(t, f, Sxx)
 		plt.ylabel('Frequency [Hz]')
 		plt.xlabel('Time [sec]')
-		plt.draw()
-		plt.pause(CHUNK/RATE)
+		plt.show(block=False)
+		plt.pause((CHUNK/SAMPLING_FREQ) * 0.5)
 
 finally:
+	plt.close()
 	stream.stop_stream()
 	stream.close()
 	p.terminate()
